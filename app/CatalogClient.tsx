74513f4713105d09e9dc8533924c5196b55ec3bc
foobarsik <olgaraat@gmail.com>
2026-08-08 20:31:44 +0200

    Use phone icon in catalog cards

--- app/CatalogClient.tsx
+++ app/CatalogClient.tsx
@@ -86,6 +86,14 @@ function WebsiteIcon() {
   );
 }
 
+function PhoneIcon() {
+  return (
+    <svg aria-hidden="true" className="phone-icon" viewBox="0 0 24 24">
+      <path d="M6.6 4.4 9 3.7l2.1 4.7-1.5 1.1c.9 1.9 2.3 3.3 4.2 4.2l1.1-1.5 4.7 2.1-.7 2.4c-.3 1-1.2 1.7-2.2 1.6C10.5 18 6 13.5 5.7 7.3c-.1-1 .6-1.9 1.6-2.2Z" />
+    </svg>
+  );
+}
+
 function ContactLink({
   href,
   children,
@@ -165,7 +173,8 @@ function SpecialistCard({
         ) : null}
         {item.phone ? (
           <ContactLink href={`tel:${item.phone.replace(/\s+/g, "")}`} external={false}>
-            Телефон
+            <span className="visually-hidden">Телефон</span>
+            <PhoneIcon />
           </ContactLink>
         ) : null}
         <button className="details-button" type="button" onClick={() => onOpen(item)}>
